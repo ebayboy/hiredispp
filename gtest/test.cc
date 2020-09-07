f778c71e4c8d3f88986ecfc6095685f59552eeee
ebayboy <ebayboy@163.com>
2020-09-07 21:12:07 +0800

    fw

---
+++ gtest/test.cc
@@ -0,0 +1,24 @@
+/****************************************************************************
+@file:test-util.cpp 
+@author:ebayboy@163.com 
+@date:2020-08-20 16:22 
+@version: 1.0  
+@description: cpp file 
+@Copyright (c)  all right reserved 
+**************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <fstream>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(protobuf, test_write)
+{
+	ASSERT_TRUE(test_write() == 0);
+}
+
